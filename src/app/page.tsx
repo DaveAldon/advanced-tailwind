6d6847f6161750660137bb9a17ec44461abcd5a7
David Crawford <crawford@mail.gvsu.edu>
2023-07-20 15:55:53 -0400

    Added tailwind integration and example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import NextJsLanding from "./Home/page";
 
-export default function Home() {
-  return <NextJsLanding />;
-}
+const Home = () => {
+  const minSize = 30;
+  return (
+    <div className="w-full">
+      <PanelGroup direction="horizontal">
+        <Panel minSize={minSize}>
+          <NextJsLanding />
+        </Panel>
+        <PanelResizeHandle className="w-[2px] cursor-col-resize bg-stone-300 dark:bg-stone-600" />
+        <Panel minSize={minSize}>
+          <NextJsLanding />
+        </Panel>
+      </PanelGroup>
+    </div>
+  );
+};
+
+export default Home;
